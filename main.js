f406ea039fd7b4296ececc7a5cb57a9f729a536d
lukeSaldanha <157385614+lukeSaldanha@users.noreply.github.com>
2024-08-05 19:09:22 -0400

    hi

---
+++ main.js
@@ -0,0 +1,69 @@
+
+/*
+	Name: Luke Saldanha
+  Student Number: 100911001
+	File: INFT 1206 Assignment 5
+	Date: April 15, 2024
+  Description: Troubleshoot the following website so that it includes web accessibility.
+	*/
+
+// functionality for showing/hiding the comments section
+
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
+
+commentWrapper.style.display = 'none';
+
+showHideBtn.onclick = function() {
+  let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+};
+
+showHideBtn.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    let showHideText = showHideBtn.textContent;
+    if(showHideText === 'Show comments') {
+      showHideBtn.textContent = 'Hide comments';
+      commentWrapper.style.display = 'block';
+    } else {
+      showHideBtn.textContent = 'Show comments';
+      commentWrapper.style.display = 'none';
+    }
+  }
+});
+
+// functionality for adding a new comment via the comments form
+
+const form = document.querySelector('.comment-form');
+const nameField = document.querySelector('#name');
+const commentField = document.querySelector('#comment');
+const list = document.querySelector('.comment-container');
+
+form.onsubmit = function(e) {
+  e.preventDefault();
+  submitComment();
+};
+
+function submitComment() {
+  const listItem = document.createElement('li');
+  const namePara = document.createElement('p');
+  const commentPara = document.createElement('p');
+  const nameValue = nameField.value;
+  const commentValue = commentField.value;
+
+  namePara.textContent = nameValue;
+  commentPara.textContent = commentValue;
+
+  list.appendChild(listItem);
+  listItem.appendChild(namePara);
+  listItem.appendChild(commentPara);
+
+  nameField.value = '';
+  commentField.value = '';
+}
